35c0171446c607c63cf40c5391348ff69f93f446
Myshchyshyn Oleksandr <vbobiby.my@gmail.com>
2018-04-28 11:46:30 +0300

    Delete unusable code

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import 'bootstrap/dist/css/bootstrap.css';
-
 import ProductItem from '../ProductItem/productItem';
 
 class Product extends Component {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import './components/Product/product';
 import Product from './components/Product/product';
 
 ReactDOM.render(<Product />, document.getElementById('root'));
